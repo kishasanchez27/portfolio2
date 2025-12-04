e6f96b2816341b36bfcbef2edf346538d5757c6d
Kisha Ann Joy M. Sanchez <kishasanchez475@gmail.com>
2025-12-04 23:00:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const toggleButton = document.getElementById('theme-toggle');
+const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
-toggleButton.addEventListener('click', () => {
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+    body.classList.add('dark');
+    themeToggle.textContent = 'Light Mode';
+}
+
+themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark');
+    
+    if (body.classList.contains('dark')) {
+        themeToggle.textContent = 'Light Mode';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        themeToggle.textContent = 'Dark Mode';
+        localStorage.setItem('theme', 'light');
+    }
 });
